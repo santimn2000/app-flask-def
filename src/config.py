b7f8d73c6b324fc0cd811d18f1fcb49a9e351a3c
santimn2000 <130979889+santimn2000@users.noreply.github.com>
2024-01-22 10:08:34 +0100

    Delete src directory

--- src/config.py
+++
@@ -1,5 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'mysql://santimn:root@localhost/gesjuegos'
-
-
-SQLALCHEMY_DATABASE_URI = 'mysql://santimn:rootroot@db4free.net/iesdaw45'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
